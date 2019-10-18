8ade1b775d5e79e21c66bb3f40b5839c98ddc191
Lã Xuân Linh <laxuanlong@gmail.com>
2019-10-18 13:25:22 +0700

    question 3

---
+++ src/main/java/com/linhlx/singtelTest/model/Rooster.java
@@ -0,0 +1,9 @@
+package com.linhlx.singtelTest.model;
+
+public class Rooster extends Chicken{
+
+    @Override
+    public void makeSound() {
+        System.out.println("Cock-a-doodle-doo");
+    }
+}
