deb9ba0fda44c2960aa8772a549b599a1951f35f
Lã Xuân Linh <laxuanlong@gmail.com>
2019-10-18 12:52:04 +0700

    add some files

---
+++ src/test/java/com/linhlx/singtelTest/SingtelTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhlx.singtelTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingtelTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
